18e92ebf548ef2bd4e259181c45921425166f7d2
Maximiliano Duthey <maxi.duthey@txpipe.io>
2025-04-10 12:57:24 -0300

    fix(): fix dropdown hidden on sidebar + add future support for oura versions

--- src/routeData.ts
+++ src/routeData.ts
@@ -1,13 +1,21 @@
 import { defineRouteMiddleware } from '@astrojs/starlight/route-data';
 
-export const onRequest = defineRouteMiddleware(context => {
+// NOTE: This is to support oura documentation versioning
+const versionRegexp = /^(v\d+)/; // Matches "v1", "v2", etc. -
+
+export const onRequest = defineRouteMiddleware(async (context, next) => {
+  await next();
+
   // Get the current page's entry and pagination data from Starlight
   const { entry, pagination } = context.locals.starlightRoute;
 
   // Extract the top-level docs section from the current page's ID
   // e.g. for "guides/start" this would return "guides"
-  const currentDocs = entry.id.split('/')[0];
-
+  const parts = entry.id.split('/');
+  let currentDocs = parts[0];
+  if (versionRegexp.test(parts[1])) {
+    currentDocs += '/' + parts[1];
+  }
   // Remove the "previous" pagination link if it points to a different docs section
   if (pagination.prev && !pagination.prev.href.includes(currentDocs)) {
     pagination.prev = undefined;
